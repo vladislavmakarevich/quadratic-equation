b390b1e60c75369c5d4282ca8a239ef9726c489f
vladislavmakarevich <bgmakar79@gmail.com>
2018-09-22 03:41:46 +0300

    Implement func Quadratic equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  var spl=equation.split(" ");
+  var a = spl[0];
+  var b = spl[3] + spl[4];
+  var c = spl[7] + spl[8];
+  var x1=Math.round((-b+Math.sqrt(b*b-4*a*c))/(2*a));
+  var x2=Math.round((-b-Math.sqrt(b*b-4*a*c))/(2*a));
+  var answer = [];
+  answer.push(x1);
+  answer.push(x2);
+  if(answer[0]>answer[1]){
+    answer.reverse();
+  }
+  return answer;
 }
